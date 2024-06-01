87546eca6c21daa457817c70f6a59e6921474cdd
jordan <jordan@aureaimaging.com>
2024-06-01 13:14:02 +0200

    fine tune script

--- package_statistics.py
+++ package_statistics.py
@@ -6,7 +6,7 @@ import gzip
 from collections import defaultdict
 
 MIRROR_URL = "http://ftp.uk.debian.org/debian/dists/stable/main/"
-TOP_N = 10
+TOP_N_PACKAGES = 10
 
 def download_contents_file(architecture):
     """
@@ -24,21 +24,39 @@ def parse_contents(contents):
     Parse the Contents file and count the number of files associated with each package.
     """
     package_file_count = defaultdict(int)
+
+    # Split the file contents based on new lines.
     for line in contents.splitlines():
-        parts = line.split()
-        if len(parts) < 2:
-            continue  # ignore all rows which do not have at least the first 2 columns filled in
-        packages = parts[1].split(',')  # parse packages if available
+
+        # Get the file path (including any whitespace characters) and the package(s).
+        # We expect 2 values to unpack due to 2 columns, otherwise ignore line.
+        try:
+            filename, packages = line.rsplit(maxsplit=1)
+        except ValueError:
+            continue
+
+        # Ignore leading dot in filename
+        if filename.startswith('.'):
+            continue
+
+        # Split package(s) based on comma and count up in dictionary
+        packages = packages.split(',')
         for package in packages:
             package_file_count[package] += 1
+
+    # Return a dictionary where keys are package names and
+    # values are counts of files associated with those packages
     return package_file_count
 
 def get_top_packages(package_file_count):
     """
     Get the top N packages with the most files.
     """
+
+    # Convert dictionary into a list of tuples and sort this list based
+    # on the second element of each tuple, representing file count
     sorted_packages = sorted(package_file_count.items(), key=lambda x: x[1], reverse=True)
-    return sorted_packages[:TOP_N]
+    return sorted_packages[:TOP_N_PACKAGES]
 
 def main():
     parser = argparse.ArgumentParser(description='Tool for Debian package file statistics')
