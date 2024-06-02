5c5e084823d4ddaafc2b544f99ce5c16471f7770
jordan <jordan@aureaimaging.com>
2024-06-02 10:57:41 +0200

    linting and formatting

--- package_statistics.py
+++ package_statistics.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 
+"""Script utility for showing statistics of deb index files"""
+
 import argparse
-import requests
-import gzip
 from collections import defaultdict
+import gzip
+import requests
+
 
 MIRROR_URL = "http://ftp.uk.debian.org/debian/dists/stable/main/"
 TOP_N_PACKAGES = 10
+
 
 def download_contents_file(architecture):
     """
     Download and decompress the Contents file for the given architecture from the Debian mirror.
     """
     url = f"{MIRROR_URL}Contents-{architecture}.gz"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return gzip.decompress(response.content).decode('utf-8')
-    else:
-        raise Exception(f"Failed to download Contents file for architecture: {architecture}\nResponse code: {response.status_code}")
+    response = requests.get(url, timeout=20)
+    if response.status_code != 200:
+        # pylint: disable=broad-exception-raised
+        raise Exception(
+            f"Failed to download Contents file for architecture: \
+                {architecture}\nResponse code: {response.status_code}"
+        )
+    return gzip.decompress(response.content).decode("utf-8")
+
 
 def parse_contents(contents):
     """
@@ -36,17 +44,18 @@ def parse_contents(contents):
             continue
 
         # Ignore leading dot in filename
-        if filename.startswith('.'):
+        if filename.startswith("."):
             continue
 
         # Split package(s) based on comma and count up in dictionary
-        packages = packages.split(',')
+        packages = packages.split(",")
         for package in packages:
             package_file_count[package] += 1
 
     # Return a dictionary where keys are package names and
     # values are counts of files associated with those packages
     return package_file_count
+
 
 def get_top_package_occurence(package_file_count):
     """
@@ -55,16 +64,26 @@ def get_top_package_occurence(package_file_count):
 
     # Convert dictionary into a list of tuples and sort this list based
     # on the second element of each tuple, representing file count
-    sorted_packages = sorted(package_file_count.items(), key=lambda x: x[1], reverse=True)
+    sorted_packages = sorted(
+        package_file_count.items(), key=lambda x: x[1], reverse=True
+    )
 
     # Return only the top N occuring packages and their file count
     return sorted_packages[:TOP_N_PACKAGES]
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Tool for Debian package file statistics')
-    parser.add_argument('architecture', help='Architecture (e.g. amd64, arm64, i386 etc.)')
+    """
+    Main script function.
+    """
+    parser = argparse.ArgumentParser(
+        description="Tool for Debian package file statistics"
+    )
+    parser.add_argument(
+        "architecture", help="Architecture (e.g. amd64, arm64, i386 etc.)"
+    )
     args = parser.parse_args()
-    
+
     try:
         contents = download_contents_file(args.architecture)
         package_file_count = parse_contents(contents)
@@ -73,8 +92,10 @@ def main():
         for package, file_count in top_packages:
             print(f"{package}\t{file_count}")
 
+    # pylint: disable=broad-exception-caught
     except Exception as e:
         print(f"Error: {e}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test_package_statistics.py
+++ test_package_statistics.py
@@ -1,6 +1,13 @@
-import pytest
+"""
+Testfile for package statistics script.
+"""
+
+# pylint: disable=missing-function-docstring, redefined-outer-name
+
 from collections import defaultdict
+import pytest
 from package_statistics import parse_contents, get_top_package_occurence
+
 
 @pytest.fixture
 def sample_contents():
@@ -13,13 +20,11 @@ usr/sbin/baz packageC,packageA
 file/without/package
 """
 
+
 @pytest.fixture
 def expected_statistics():
-    return defaultdict(int, {
-        'packageA': 2,
-        'packageB': 3,
-        'packageC': 1
-    })
+    return defaultdict(int, {"packageA": 2, "packageB": 3, "packageC": 1})
+
 
 def test_parse_contents(sample_contents, expected_statistics):
     result = parse_contents(sample_contents)
@@ -28,5 +33,5 @@ def test_parse_contents(sample_contents, expected_statistics):
 
 def test_get_top_packages(expected_statistics):
     result = get_top_package_occurence(expected_statistics)
-    sorted_statistics = [('packageB', 3),('packageA', 2),('packageC', 1)]
+    sorted_statistics = [("packageB", 3), ("packageA", 2), ("packageC", 1)]
     assert result == sorted_statistics
